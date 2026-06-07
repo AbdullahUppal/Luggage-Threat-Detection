cd1fe3431149593dcf737bb0989192f090391d27
Abdullah Uppal <123026674+AbdullahUppal@users.noreply.github.com>
2026-06-07 17:51:30 +0500

    Added the segmentation layer imlpementation in processing calling for trainig and if trained model found then using it instead of training.
    in Windows GPU usage isn't supported with tensorflow

--- processing.py
+++ processing.py
@@ -1,18 +1,32 @@
-import cv2 as cv
-from config.classification_model import Classification_Model
+from config.segementation_model import SegmentationModel
 import os
-from constants import TRAIN_CLASSIFICATION, TEST
+from constants import TRAIN, TRAIN_ANNOTATION
 class Processing():
     # This init will act as the main process function of the class
     def __init__(self):
-        self.classify_image()
+        self.segment_image()
+        # self.classify_image()
         
+    def segment_image(self):
+        segment_model = SegmentationModel()
+        if not os.path.exists("model/segmentation_model.pt"):
+            segment_model.train_segmentation(
+                train_root=TRAIN,
+                annotation_root=TRAIN_ANNOTATION,
+                epochs=20,
+                batch_size=8
+            )
+        segment_model.load_model()
+
     def classify_image(self):
+        from config.classification_model import Classification_Model
         model = Classification_Model()
-        if not os.path.exists("model/classification_model.h5"):
-            model.train(TRAIN_CLASSIFICATION)
+        print("\nTrain: ", TRAIN)
+        if not os.path.exists("model/classification_model.pt"):
+            model.train(TRAIN)
         model.load_model()
-        model.predict_class(TEST)
+        # model.predict_class(TEST)
+
 
         
 
